55ed79866b7610d7b75013901ec4d1c19b59031b
Process-ing <rbruno.p.o@gmail.com>
2024-09-16 07:36:05 +0100

    Fix nvidia modules placement

--- setup-scripts/nvidia.sh
+++ setup-scripts/nvidia.sh
@@ -5,7 +5,7 @@ function setup_nvidia() {
     install_official nvidia-dkms
 
     log "Setting up NVIDIA modules..."
-    sudo sed -i '/^MODULES=/s/)/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/; /s/=( /=(/' /etc/mkinitcpio.conf 
+    sudo sed -i '/^MODULES=/s/)/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/; s/=( /=(/' /etc/mkinitcpio.conf 
     # Second script is to remove the first space if the array is initially empty
     sudo mkinitcpio -P
 }
