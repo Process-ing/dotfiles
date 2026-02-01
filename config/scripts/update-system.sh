2a1be036a5ba9e9755b88bf3c29625532743e25c
Process-ing <rbruno.p.o@gmail.com>
2026-02-01 14:08:59 +0000

    Add error message to update script

--- config/scripts/update-system.sh
+++ config/scripts/update-system.sh
@@ -2,8 +2,11 @@
 
 COMMANDS=$(cat << EOF
 fastfetch
-yay
-notify-send "System updated!" -i $HOME/.config/icons/update.png
+if yay --sudoloop; then
+	notify-send "System updated!" -i $HOME/.config/icons/update.png
+else
+	notify-send 'System failed to update. Please try again.'
+fi
 read -p 'Press ENTER to continue...'
 EOF
 )
