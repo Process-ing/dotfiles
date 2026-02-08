3f9f4cb8d8c2c3c40bee809854f13d65c414d7e3
Process-ing <rbruno.p.o@gmail.com>
2026-02-08 20:17:46 +0000

    Fix update check script

--- setup.sh
+++ setup.sh
@@ -83,7 +83,7 @@ official_packages=("base-devel"                                                 
 )
 
 aur_packages=("visual-studio-code-bin" "cmatrix-git" "i3lock-color" "peaclock"
-    "adwaita-qt5-git" "adwaita-qt6-git" "checkupdates-with-aur")
+    "adwaita-qt5-git" "adwaita-qt6-git" "aur-check-updates-bin")
 
 log "Configuring pacman..."
 sudo_place $ROOT/config/pacman/pacman.conf /etc/pacman.conf
