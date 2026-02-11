68edcd9da8e025c5aa75f139c0304496bd72aaf5
Process-ing <rbruno.p.o@gmail.com>
2026-02-11 08:04:45 +0000

    Fix update checking script

--- config/scripts/updates.sh
+++ config/scripts/updates.sh
@@ -4,7 +4,7 @@
 
 NOTIFY_ICON=/usr/share/icons/Papirus/32x32/apps/system-software-update.svg
 
-get_total_updates() { UPDATES=$(aur-check-updates --raw 2>/dev/null | wc -l); }
+get_total_updates() { UPDATES=$(aur-check-updates --repos extra --raw 2>/dev/null | wc -l); }
 
 while true; do
     get_total_updates
